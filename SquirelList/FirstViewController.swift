a2025743dca95a30060192485e2dd1ef5184873d
dgl1230 <dgearylopez@gmail.com>
2015-01-25 12:49:22 -0800

    Initial Commit

---
+++ SquirelList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SquirelList
+//
+//  Created by Denis Geary Lopez on 1/25/15.
+//  Copyright (c) 2015 Frenvu Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
